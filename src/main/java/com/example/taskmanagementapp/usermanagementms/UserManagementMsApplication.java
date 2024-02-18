904f52f6131fd8ae68da732113141bcb7a790ecb
Pramod Kolte <pramodkoltepatil@gmail.com>
2024-02-18 19:45:58 +0530

    Initial Commit

---
+++ src/main/java/com/example/taskmanagementapp/usermanagementms/UserManagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementapp.usermanagementms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskmanagementapp/usermanagementms/UserManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementapp.usermanagementms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
